a568d7c95c6fdbe6478feab404be8138e972ae6b
Albayrakengin <engy.albayrak@gmail.com>
2024-08-04 16:07:15 +0300

    pushing data into kafka topic

--- news_scrape/news_scrape/spiders/newspider.py
+++ news_scrape/news_scrape/spiders/newspider.py
@@ -4,12 +4,22 @@ import scrapy
 from scrapy.http import Response
 import os
 from dotenv import load_dotenv
+from kafka import KafkaProducer
+import json
 
 load_dotenv()
 
 class NewSpider(scrapy.Spider):
     name = "newspider"
     page_counter = 0
+
+    def __init__(self, *args, **kwargs):
+        super(NewSpider, self).__init__(*args, **kwargs)
+        self.producer = KafkaProducer(   
+        bootstrap_servers=['broker:29092'],
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+
     def start_requests(self):
         url = os.getenv('NEWS_URL')
         tag = getattr(self, "tag", None)
@@ -25,12 +35,16 @@ class NewSpider(scrapy.Spider):
         for titles in response.xpath("//a[@class='card-title']/text()").getall():
             description = descArray[i]
             i += 1 
-            yield {
+            
+            data = {
                 "title": titles,
                 "description": description
             }
+            self.producer.send('ratatoy', value=data)
+            yield data
 
         next_page = response.xpath("//meta[@property='og:url']/@content").get()
         next_page = f"{next_page}{self.page_counter}"
+        
         if (next_page is not None) and (self.page_counter <= 10):
             yield response.follow(next_page, self.parse)
